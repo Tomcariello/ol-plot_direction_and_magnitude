7d29e15499d2e3b5e0565a028023761c6b1949af
Tom Cariello <tomcariello@gmail.com>
2022-11-18 16:01:35 -0500

    Distilled code, added lots of comments

--- main.js
+++ main.js
@@ -3,95 +3,40 @@ import Map from 'ol/Map';
 import Point from 'ol/geom/Point';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
+import Tile from 'ol/layer/Tile';
 import View from 'ol/View';
 import {Fill, RegularShape, Stroke, Style} from 'ol/style';
 import OSM from 'ol/source/OSM';
-import Tile from 'ol/layer/Tile';
+import {fromLonLat} from 'ol/proj';
 
+// An array to contain all of the features from the dataset after processing
+const featuresArr = [];
 
-// This determines the number to points to generate
-const count = 250;
-const features = new Array(count);
-const e = 4500000;
+// Set of colors to be applied to each feature based on the feature's magnitude
+const arrowColorScaleArr = ["red","blue","green"];
 
-const fillArr = ["red","blue","green"];
+// Fetch data, convert to JSON, then processData()
+const getData = await fetch('data/spread_weather.json')
+  .then(function (resData) {
+    return resData.json();
+  })
+  .then(function (resDataJSON) {
+    processData(resDataJSON);
+  });
 
-// Create random coordinates, direction & magnitude
-for (let i = 0; i < count; ++i) {
-  const coordinates = [6 * e * Math.random() - e, 6 * e * Math.random() - e];
-  
-  // Randomly generate DEGREES & convert to RADIANS
-  // Surely there is a more direct approach...
-  const pi = 3.14
-  const radianDirection = Math.floor(Math.random() * 361) * pi/180;
-  
-  // Generate the magnitude
-  const magnitude = Math.floor(Math.random() * 20000);
-  console.log(magnitude)
-
-  // Select random color from array
-  // const arrowColor = fillArr[Math.floor(Math.random() * (fillArr.length))]
-  let arrowColor = fillArr[0];
-  let arrowMultiplier = 1;
-
-  // Based on magnitude, adjust color & arrow length
-  if (magnitude < 6666) {
-    arrowColor = fillArr[1];
-    arrowMultiplier = 1.25
-  } else if (magnitude < 13500) {
-    arrowColor = fillArr[2];
-    arrowMultiplier = 1.5
-  }
-
-  // Create a style array containing an arrow shaft & arrow point
-  const arrow = [
-    // the arrow shaft
-    new Style({
-      image: new RegularShape({
-        points: 2,
-        radius: 10 * arrowMultiplier,
-        stroke: new Stroke({
-          width: 2,
-          color: arrowColor,
-        }),
-        displacement: [0,0], // [horizontal offset, vertical offset] positive shifts right or down
-        angle: radianDirection,
-      }),
-    }),
-    // The arrow head <-- This does render!
-    new Style({
-        image: new RegularShape({
-          points: 3,
-          radius: 5 * arrowMultiplier,
-          displacement: [0,0],
-          angle: radianDirection,
-          fill: new Fill({color: arrowColor}),
-        }),
-      })
-    ];
-
-  // Apply feature & style
-  features[i] = new Feature(new Point(coordinates));
-  features[i].setStyle(arrow);
-}
-
-// Apply all features from array to a VectorSource
-const source = new VectorSource({
-  features: features,
-});
-
-// Apply the source to a VectorLayer 
-const vectorLayer = new VectorLayer({
-  source: source,
-});
-
-// Initialize the map with the vectorLayer
+// Initialize the map
 const map = new Map({
   layers: [
+    // Include an Open Street Map layer
     new Tile({
       source: new OSM()
     }),
-    vectorLayer,
+    // Include a Vector Layer which contains all of our features
+    new VectorLayer({
+      source: new VectorSource({
+        features: featuresArr,
+      })
+    }),
   ],
   target: 'map',
   view: new View({
@@ -99,3 +44,67 @@ const map = new Map({
     zoom: 2,
   }),
 });
+
+function processData(data) {
+  const weatherData = data.list;
+  // Process data
+  for (let i = 0; i < weatherData.length; ++i) {
+    // Extract coordinates & wind values
+    const { coord, wind } = weatherData[i];
+
+    // Convert from lon/lat to floats using the provided OL method
+    const coordinates = fromLonLat([coord.lon, coord.lat]);
+    
+    // Convert DEGREES to RADIANS
+    const pi = 3.14
+    const radianDirection = wind.deg * pi/180;
+    
+    const magnitude = wind.speed;
+
+    // Initialize color & size multiplier values
+    let arrowColor, arrowSizeMultiplier;
+
+    // Adjust color & arrow length based on magnitude
+    if (magnitude < 15) {
+      arrowColor = arrowColorScaleArr[0];
+      arrowSizeMultiplier = 1
+    } else if (magnitude < 22) {
+      arrowColor = arrowColorScaleArr[1];
+      arrowSizeMultiplier = 1.25
+    } else {
+      arrowColor = arrowColorScaleArr[2];
+      arrowSizeMultiplier = 1.5;
+    }
+
+    // Create a style array containing an arrow shaft & arrow point
+    const arrow = [
+      // The arrow shaft
+      new Style({
+        image: new RegularShape({
+          points: 2,
+          radius: 10 * arrowSizeMultiplier,
+          stroke: new Stroke({
+            width: 2,
+            color: arrowColor,
+          }),
+          angle: radianDirection,
+        }),
+      }),
+      // The arrow head
+      new Style({
+          image: new RegularShape({
+            points: 3,
+            radius: 5 * arrowSizeMultiplier,
+            angle: radianDirection,
+            fill: new Fill({
+              color: arrowColor
+            }),
+          }),
+        })
+      ];
+
+    // Push feature & style to the array
+    featuresArr[i] = new Feature(new Point(coordinates));
+    featuresArr[i].setStyle(arrow);
+  }
+}
