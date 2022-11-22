53eb1e9208530d2623bbf56839fc9ce257ad095f
Tom Cariello <tomcariello@gmail.com>
2022-11-22 17:08:15 -0500

    Removed all WebGL stuff.

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-import Feature from 'ol/Feature';
-import Map from 'ol/Map';
-import Point from 'ol/geom/Point';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import Tile from 'ol/layer/Tile';
-import View from 'ol/View';
-import {Fill, RegularShape, Stroke, Style} from 'ol/style';
-import OSM from 'ol/source/OSM';
-import {fromLonLat} from 'ol/proj';
+import Feature from "ol/Feature";
+import Map from "ol/Map";
+import Point from "ol/geom/Point";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import Tile from "ol/layer/Tile";
+import View from "ol/View";
+import { Fill, RegularShape, Stroke, Style } from "ol/style";
+import OSM from "ol/source/OSM";
+import { fromLonLat } from "ol/proj";
 
 // An array to contain all of the features from the dataset after processing
 const featuresArr = [];
 
 // Set of colors to be applied to each feature based on the feature's magnitude
-const arrowColorScaleArr = ["red","blue","green"];
+const arrowColorScaleArr = ["red", "blue", "green"];
 
 // Fetch data, convert to JSON, then processData()
-const getData = await fetch('data/weather.json')
+const getData = await fetch("data/weather.json")
   .then(function (resData) {
     return resData.json();
   })
@@ -29,16 +29,15 @@ const map = new Map({
   layers: [
     // Include an Open Street Map layer
     new Tile({
-      source: new OSM()
+      source: new OSM(),
     }),
-    // Include a Vector Layer which contains all of our features
     new VectorLayer({
-      source: new VectorSource({
+      source: new VectorSource({ // ol/source/Vector
         features: featuresArr,
-      })
+      }),
     }),
   ],
-  target: 'map',
+  target: "map",
   view: new View({
     center: [0, 0],
     zoom: 2,
@@ -54,11 +53,10 @@ function processData(data) {
 
     // Convert from lon/lat to floats using the provided OL method
     const coordinates = fromLonLat([coord.lon, coord.lat]);
-    
+
     // Convert DEGREES to RADIANS
-    const pi = 3.14
-    const radianDirection = wind.deg * pi/180;
-    
+    const pi = 3.14;
+    const radianDirection = (wind.deg * pi) / 180;
     const magnitude = wind.speed;
 
     // Initialize color & size multiplier values
@@ -67,10 +65,10 @@ function processData(data) {
     // Adjust color & arrow length based on magnitude
     if (magnitude < 15) {
       arrowColor = arrowColorScaleArr[0];
-      arrowSizeMultiplier = 1
+      arrowSizeMultiplier = 1;
     } else if (magnitude < 22) {
       arrowColor = arrowColorScaleArr[1];
-      arrowSizeMultiplier = 1.25
+      arrowSizeMultiplier = 1.25;
     } else {
       arrowColor = arrowColorScaleArr[2];
       arrowSizeMultiplier = 1.5;
@@ -92,16 +90,16 @@ function processData(data) {
       }),
       // The arrow head
       new Style({
-          image: new RegularShape({
-            points: 3,
-            radius: 5 * arrowSizeMultiplier,
-            angle: radianDirection,
-            fill: new Fill({
-              color: arrowColor
-            }),
+        image: new RegularShape({
+          points: 3,
+          radius: 5 * arrowSizeMultiplier,
+          angle: radianDirection,
+          fill: new Fill({
+            color: arrowColor,
           }),
-        })
-      ];
+        }),
+      }),
+    ];
 
     // Push feature & style to the array
     featuresArr[i] = new Feature(new Point(coordinates));
